8c12506a49c8effae126669c7d0c3932227a5bbb
Joseph <josephkim570@gmail.com>
2020-10-28 17:12:23 -0400

    Restructured to use reducers for multiple state management.

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,62 +1,52 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 // syntax for special variable that is constant like a configuration
 const COLOR_INCREMENT = 15;
 
-const SquareScreen = () => {
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
+const reducer = (state, action) => {
 
-    // color === red, green, blue
-    // change === +15, -15
+    switch (action.colorToChange) {
+        case 'red':
+            return {...state, red: state.red + action.amount };
+            // { red: 0, green: o, blue: 0, red: state.red + action.amount}
 
-    // const setColor = (color, change) => {
-    //     if (color === 'red') {
-    //         if (red + change > 255 || red + change < 0) {
-    //             return;
-    //         } else {
-    //             setRed(Red + change);
-    //         }   
-    //     }
-    // }
+            // redefining a proprerty replaces the existing to the new one
+            // { green: 0, blue: 0 , red: state.red + action.amount}
 
-    const setColor = (color, change) => {
-        switch(color) {
-            case 'red':
-                red + change > 255 || red + change < 0 ? null : setRed(red + change);
-                return;
-            case 'green':
-                green + change > 255 || green + change < 0 ? null : setGreen(green + change);
-                return;
-            case 'blue':
-                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
-                return;
-            default:
-                return;
-        }
+        case 'green':
+            return {...state, green: state.green + action.amount };
+        case 'blue':
+            return {...state, blue: state.blue + action.amount };
+        default:
+            // in case an action results in an error or is incorrect
+            // default syntax 
+            return state;
     }
-    
+}
+
+const SquareScreen = () => {
+
+    // dispatch = runMyReducer
+    const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0});
+    const { red, green, blue } = state;
 
     return (
         <View>
             <ColorCounter 
-                // onIncrease={() => setRed(red + COLOR_INCREMENT)} 
-                // onDecrease={() => setRed(red - COLOR_INCREMENT)} 
-                onIncrease={() => setColor('red', COLOR_INCREMENT)}
-                onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
+                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })}
                 color="Red" 
             />
             <ColorCounter 
-                onIncrease={() => setColor('blue', + COLOR_INCREMENT)}
-                onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
+                onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })}
                 color="Blue" 
             />
             <ColorCounter 
-                onIncrease={() => setColor('green', + COLOR_INCREMENT)}
-                onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
+                onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })}
                 color="Green" 
             
             />
